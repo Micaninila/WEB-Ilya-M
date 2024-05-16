4d6d5c071f4f9d339d40feeef41d256692afb952
Илья Мичанин <iluhamichanin@gmail.com>
2024-05-16 11:21:54 +0300

    Error fixed (or not)

--- Lab4/grid.js
+++ Lab4/grid.js
@@ -6,7 +6,7 @@ const CELLS_COUNT = GRID_SIZE * GRID_SIZE;
 export class Grid{
     constructor(gridElement){
         this.cells = [];
-        for (let i = 0; index < CELLS_COUNT; i++){
+        for (let i = 0; i < CELLS_COUNT; i++){
             this.cells.push(
                 new Cell(gridElement, i % GRID_SIZE, Math.floor(i / GRID_SIZE))
             );
