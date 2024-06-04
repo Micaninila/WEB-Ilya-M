0ea3a4f70e3f1a03c776f96e5d4bf18eb6e1c69d
Илья Мичанин <iluhamichanin@gmail.com>
2024-06-04 20:42:40 +0300

    Task-4 done

--- Lab5/task-4/src/App.js
+++ Lab5/task-4/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const navlinkItems = [
+    { label: 'Главная', href: '/' },
+    { label: 'О нас', href: '/about' },
+    { label: 'Контакты', href: '/contacts' },
+    // add more items here
+  ];
+  
+  const content = (
+      <ul className="nav">
+        {navlinkItems.map((item, index) => (
+          <li key={index}>
+            <a href={item.href}>{item.label}</a>
+          </li>
+        ))}
+      </ul>
+  );
+  return(
+    content
   );
 }
 
